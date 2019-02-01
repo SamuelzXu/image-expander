708c2f8ddac486da95fca4f995a64c2913cf7853
William Tang <williamjt1998@gmail.com>
2019-02-01 11:36:29 -0500

    Diff stored as bmp

--- diffBMP.cpp
+++ diffBMP.cpp
@@ -2,67 +2,30 @@
 #include "bitmap_image.hpp"
 using namespace std;
 
-void expand_image(std::tuple<int, int> oldRes, std::tuple<int, int> newRes, bitmap_image oldImg, vector<vector<rgb_t>> diff) {
-    int fact = std::get<0>(newRes) * std::get<1>(newRes) / std::get<0>(oldRes) * std::get<1>(oldRes);
-    bitmap_image newImg(1280,720);
-    int newH = 720, newW = 1280;
-    if (true) {
-        for (int y=0; y<newH; ++y) {
-            if (y%2 == 0) {
-                for (int x=0; x<newW; ++x) {
-                    if (x%2 == 0) {
-                        newImg.set_pixel(x,y,oldImg.get_pixel(x/2,y*2/3));
-                    }
-                    else {
-                        newImg.set_pixel(x,y,diff.at(y).at((x-1)/2));
-                    }
-                }
-            } else {
-                for (int x=0; x<newW; ++x) {
-                    newImg.set_pixel(x,y,diff.at(y).at(x));
-                }
-            }
-        }
-    }
-    else {
-        throw std::logic_error("Not implemented for this res");
-    }
-    newImg.save_image("expanded.bmp");
-}
+void generateDiff (string lowRes, string highRes){
+   bitmap_image lowResImage(lowRes);
+   bitmap_image highResImage(highRes);
 
-vector<vector<rgb_t> > getDiff (string input){
-   vector<vector<rgb_t> > diff; 
-   bitmap_image image(input);
-   if (!image){
+ if (!highResImage){
       printf("Error - Failed to open file\n");
-      return diff;
    }
-
+   bitmap_image image(highResImage.width(),highResImage.height());
 
    const unsigned int height = image.height();
    const unsigned int width  = image.width();
    for (std::size_t y = 0; y < height; ++y){
-         vector<rgb_t> row;
       for (std::size_t x = 0; x < width; ++x){
          if (y % 2 == 1 || x % 2 == 1){
             rgb_t colour;
-            image.get_pixel(x, y, colour);
-            row.push_back(colour);
+            highResImage.get_pixel(x, y, colour);
+            image.set_pixel(x, y, colour);
          }
       }
-      diff.push_back(row);
    }
-   return diff;
+   image.save_image("diff-720.bmp");
 }
 
 
 int main(int argc, char *argv[]){
-   vector<vector<rgb_t> > diff; 
-   diff = getDiff(argv[1]);
-   // for (auto &it : diff){
-   //    cout<<it.size()<<endl;
-   // }
-   // cout<<diff.size()<<endl;
-   bitmap_image old("480p.bmp");
-   expand_image(std::make_tuple<int, int>(480,640),std::make_tuple<int, int>(720, 1280), old, diff);
+   generateDiff("480p.bmp", "720p.bmp");
 }
---
+++ expand_image.cc
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include "bitmap_image.hpp"
+using namespace std;
+
+void expand_image(bitmap_image oldImg, bitmap_image diff) {
+    bitmap_image newImg(1280,720);
+    int newH = 720, newW = 1280;
+    if (true) {
+        for (int y=0; y<newH; ++y) {
+            if (y%2 == 0) {
+                for (int x=0; x<newW; ++x) {
+                    if (x%2 == 0) {
+                        newImg.set_pixel(x,y,oldImg.get_pixel(x/2,y*2/3));
+                    }
+                    else {
+                        newImg.set_pixel(x,y,diff.get_pixel(x,y));
+                    }
+                }
+            } else {
+                for (int x=0; x<newW; ++x) {
+                    newImg.set_pixel(x,y,diff.get_pixel(x,y));
+                }
+            }
+        }
+    }
+    else {
+        throw std::logic_error("Not implemented for this res");
+    }
+    newImg.save_image("expanded.bmp");
+}
+
+int main(int argc, char *argv[]){
+    bitmap_image diff(argv[1]);
+    bitmap_image old(argv[2]);
+    expand_image(old, diff);
+}
